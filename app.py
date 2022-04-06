61a427f625b6b2fcdaba6dbbd6cc69d06a803406
Beannoy <lovepatel9925@gmail.com>
2022-04-06 19:15:42 -0400

    c

--- app.py
+++ app.py
@@ -28,14 +28,15 @@ nlp_model = NeuralNet(input_size, hidden_size, output_size).to(device)
 nlp_model.load_state_dict(model_state)
 nlp_model.eval()
 
-description_dis = pd.read_csv("data/symptom_Description.csv")
-description_dis['Disease'] = description_dis['Disease'].apply(lambda x: x.lower().strip(" "))
+diseases_description = pd.read_csv("data/symptom_Description.csv")
+diseases_description['Disease'] = diseases_description['Disease'].apply(lambda x: x.lower().strip(" "))
 
-precaution = pd.read_csv("data/symptom_precaution.csv")
-precaution['Disease'] = precaution['Disease'].apply(lambda x: x.lower().strip(" "))
+disease_precaution = pd.read_csv("data/symptom_precaution.csv")
+disease_precaution['Disease'] = disease_precaution['Disease'].apply(lambda x: x.lower().strip(" "))
 
-symp_severity = pd.read_csv("data/Symptom-severity.csv")
-symp_severity = symp_severity.applymap(lambda s: s.lower().strip(" ").replace(" ", "") if type(s) == str else s)
+symptom_severity = pd.read_csv("data/Symptom-severity.csv")
+symptom_severity = symptom_severity.applymap(lambda s: s.lower().strip(" ").replace(" ", "") if type(s) == str else s)
+
 
 with open('data/list_of_symptoms.pickle', 'rb') as data_file:
     symptoms_list = pickle.load(data_file)
@@ -75,6 +76,7 @@ def index():
 
     return render_template('index.html', data=data)
 
+
 @app.route('/symptom', methods=['GET', 'POST'])
 def predict_symptom():
     print("Request json:", request.json)
@@ -84,7 +86,7 @@ def predict_symptom():
         if not user_symptoms:
             response_sentence = random.choice(
                 ["I can't know what disease you may have if you don't enter any symptoms :)",
-                "MedAI can't know the disease if there are no symptoms...",
+                "Meddy can't know the disease if there are no symptoms...",
                 "You first have to enter some symptoms!"])
         else:
             x_test = []
@@ -94,23 +96,23 @@ def predict_symptom():
                     x_test.append(1)
                 else: 
                     x_test.append(0)
-            x_test = np.asarray(x_test)            
 
+            x_test = np.asarray(x_test)            
             disease = prediction_model.predict(x_test.reshape(1,-1))[0]
             print(disease)
 
-            description = description_dis.loc[description_dis['Disease'] == disease.strip(" ").lower(), 'Description'].iloc[0]
-            precaution = precaution[precaution['Disease'] == disease.strip(" ").lower()]
+            description = diseases_description.loc[diseases_description['Disease'] == disease.strip(" ").lower(), 'Description'].iloc[0]
+            precaution = disease_precaution[disease_precaution['Disease'] == disease.strip(" ").lower()]
             precautions = 'Precautions: ' + precaution.Precaution_1.iloc[0] + ", " + precaution.Precaution_2.iloc[0] + ", " + precaution.Precaution_3.iloc[0] + ", " + precaution.Precaution_4.iloc[0]
             response_sentence = "It looks to me like you have " + disease + ". <br><br> <i>Description: " + description + "</i>" + "<br><br><b>"+ precautions + "</b>"
             
             severity = []
 
             for each in user_symptoms: 
-                severity.append(symp_severity.loc[symp_severity['Symptom'] == each.lower().strip(" ").replace(" ", ""), 'weight'].iloc[0])
+                severity.append(symptom_severity.loc[symptom_severity['Symptom'] == each.lower().strip(" ").replace(" ", ""), 'weight'].iloc[0])
                 
             if np.mean(severity) > 4 or np.max(severity) > 5:
-                response_sentence = response_sentence + "<br><br>Considering your symptoms are severe, and MedAI isn't a real doctor, you should consider talking to one. :)"
+                response_sentence = response_sentence + "<br><br>Considering your symptoms are severe, and Meddy isn't a real doctor, you should consider talking to one. :)"
 
             user_symptoms.clear()
             severity.clear()
